a397ffa9356b508c448f5d2ee98ba724850527c1
Michael Gaganov <gaganov@gmail.com>
2017-04-18 10:13:01 +0300

    fixed getting remote_ip from HTTP_X_FORWARDED_FOR

--- app/models/shortener/shortened_click.rb
+++ app/models/shortener/shortened_click.rb
@@ -9,7 +9,7 @@ class Shortener::ShortenedClick < ActiveRecord::Base
   def track env
     # logger.info(env)
 
-    self.remote_ip = (env["HTTP_X_FORWARDED_FOR"].to_s.split(', ').first || env["REMOTE_ADDR"]).to_s
+    self.remote_ip = (env["HTTP_X_FORWARDED_FOR"].to_s.split(',').first.try(:strip) || env["REMOTE_ADDR"]).to_s
     self.referer = env["HTTP_REFERER"].to_s.encode("iso-8859-1").force_encoding("utf-8")
     self.agent = env["HTTP_USER_AGENT"].to_s.encode("iso-8859-1").force_encoding("utf-8")
     self.country = geo_ip.country(self.remote_ip).country_name.to_s
